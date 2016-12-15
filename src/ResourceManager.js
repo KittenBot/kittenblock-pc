322ec894e040b08b1c63a32ce72fc6e6af9ac6fb
Riven <4771215@qq.com>
2016-12-15 22:34:27 +0800

    add resource server

---
+++ src/ResourceManager.js
@@ -0,0 +1,63 @@
+/**
+ * Created by Riven on 2016/12/15.
+ */
+
+
+fs = require('fs');
+http = require('http');
+url = require('url');
+var crypt = require('crypto');
+
+var ResourceServer = function(){
+    this._server = null;
+
+};
+
+module.exports = ResourceServer;
+
+ResourceServer.prototype.getSpriteSkin = function(spriteId){
+    var targets = window.vm.runtime.targets;
+    for(var i=0;i<targets.length;i++){
+        var ele = targets[i];
+        if(ele.id==spriteId){
+            var skin = ele.sprite.costumes[0].skin;
+            return skin;
+        }
+    }
+    return "";
+};
+
+ResourceServer.prototype.startServer = function(){
+    this._server = http.createServer(function (req, res) {
+        var request = url.parse(req.url, true);
+        var action = request.pathname;
+        //console.log("server: " + action);
+
+        if (action.indexOf("png") > -1 ) {
+            var img = fs.readFileSync("./workspace/" + action.substr(1)); // remove slash
+            res.writeHead(200, {'Content-Type': 'image/png'});
+            res.end(img, 'binary');
+        }else if(action.indexOf("svg") > -1){
+            var img = fs.readFileSync("./workspace/" + action.substr(1)); // remove slash
+            res.writeHead(200, {'Content-Type': 'image/svg+xml'});
+            res.end(img, 'binary');
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Hello World \n');
+        }
+    });
+
+    this._server.on("clientError", function (err, socket) {
+        console.log("client error " + err);
+        socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+    });
+
+    this._server.on('listening', function () {
+        console.log('resource server is running');
+    });
+
+    this._server.listen(9234);
+
+};
+
+
---
+++ test/test.resource.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Riven on 2016/12/15.
+ */
+
+const ResourceManager = require('../src/ResourceManager');
+var mng = new ResourceManager();
+mng.startServer();
+
+
