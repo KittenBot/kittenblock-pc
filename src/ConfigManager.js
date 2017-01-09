b7da2d11f7636902f399c056c08893bcd21b2d52
Riven <4771215@qq.com>
2017-01-09 18:16:36 +0800

    generate template index.html

--- src/ConfigManager.js
+++ src/ConfigManager.js
@@ -4,6 +4,7 @@
 "use strict";
 
 var fs = require('fs');
+var path = require('path');
 
 var ConfigManager = function () {
     this.configFile = process.cwd()+"/kittenblock.json";
@@ -14,12 +15,31 @@ ConfigManager.prototype.load = function () {
     return JSON.parse(s);
 };
 
+ConfigManager.prototype.generateIndexHtml = function(lang){
+    var template = '<!doctype html>\n'+
+        '<html>\n'+
+        '<head>\n'+
+        '<meta charset="utf-8">\n'+
+        '<title>KittenBlock</title>\n'+
+        '</head>\n'+
+        '<body>\n'+
+        '<script type="text/javascript" src="language/en.js"></script>\n'+
+        '<script type="text/javascript" src="lib.min.js"></script><script type="text/javascript" src="gui.js"></script></body>\n'+
+        '</body>\n'+
+        '</html>\n';
+    template = template.replace("en.js",lang);
+    var s = fs.writeFileSync("app/index.html", template);
+
+};
+
 ConfigManager.prototype.save = function (config) {
+    this.generateIndexHtml(config.language.file);
     var configstr = JSON.stringify(config);
     var s = fs.writeFileSync(this.configFile, configstr);
 };
 
 module.exports = ConfigManager;
+
 
 
 
